1f64c85f3f291b0bc51a4246ae4e14c98b77c639
Isaac Lebwohl-Steiner <Isaac Lebwohl-Steiner>
2016-10-26 12:41:49 -0200

    More validator fixes

--- resources/views/lorem/create.blade.php
+++ resources/views/lorem/create.blade.php
@@ -5,36 +5,36 @@
 @stop
 
 @section('content')
+<div class="container">
 <form action="/lorem" method="POST">
 	{{ csrf_field() }}
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<div class="input-group input-group-lg">
-			      <span class="input-group-btn">
-			        <button class="btn dark btn-secondary" name="loremType" value="sentences" type="submit">Generate sentences!</button>
-			      </span>
-			      <input type="number" name="count" class="form-control dark" placeholder="How much Lorem?">
-			      <span class="input-group-btn">
-			        <button class="btn dark btn-secondary" name="loremType" value="paragraphs" type="submit">Generate paragraphs!</button>
-			      </span>
-			    </div>
-			</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="input-group input-group-lg">
+		      <span class="input-group-btn">
+		        <button class="btn dark btn-secondary" name="loremType" value="sentences" type="submit">Generate sentences!</button>
+		      </span>
+		      <input type="number" name="count" class="form-control dark" placeholder="How much Lorem?">
+		      <span class="input-group-btn">
+		        <button class="btn dark btn-secondary" name="loremType" value="paragraphs" type="submit">Generate paragraphs!</button>
+		      </span>
+		    </div>
 		</div>
+	</div>
 
-		@if(!empty($errors))
-		<div class="row">
-		@foreach ($errors->all() as $error)
-			<div class="col-xs-12">
-				<div class="alert alert-danger" role="alert">
-				  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-				  <span class="sr-only">Error:</span>
-				  {{ $error }}
-				</div>
+	@if(!empty($errors))
+	<div class="row">
+	@foreach ($errors->all() as $error)
+		<div class="col-xs-12">
+			<div class="alert alert-danger" role="alert">
+			  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			  <span class="sr-only">Error:</span>
+			  {{ $error }}
 			</div>
-		@endforeach
 		</div>
-		@endif
+	@endforeach
+	</div>
+	@endif
 </form>
 
 	@if(!empty($lorem))
@@ -46,6 +46,6 @@
 			@endforeach
 		</div>
 	@endif
-	</div>
+</div>
 @stop
 
